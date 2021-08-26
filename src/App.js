87fac59d607e61b5b6075b02aa78dc366f383ecb
Connor Wilson <cnwilson29@gmail.com>
2021-08-26 10:29:57 -0400

    star battles v3

--- src/App.js
+++ src/App.js
@@ -54,16 +54,18 @@ class App extends Component {
           }}></div>
           {/* <h1>About HyperHacks</h1> */}
           <div style={{
-            transform: 'rotate3d(1, 0, 0, 50deg)',
-            color: '#FFCC4D'
+            paddingLeft: '15%',
+            paddingRight: '15%',
+            color: '#FFCC4D',
+            perspective: '400px'
           }}>
-          <h3 className='px-5' style={{
-            lineHeight: '43px'
+          <h2 className='' style={{
+            transform: 'rotateX(25deg)',
           }}>
             Start Date: Oct 1st, 2021 <br></br>
             Length: 48hrs <br></br><br></br>
           HyperHacks is an online event where software and hardware developers from around the world form teams to create a project in only 48 hours. Our event is focused on innovation. We want to inspire students to create a unique project that brings value to society. Through our workshops, we want to give participants the knowledge to take their project further after the hackathon ends.
-          </h3>
+          </h2>
           </div>
         </div>
         <Sponsors></Sponsors>
