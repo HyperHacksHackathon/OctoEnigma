95f96ada5ad9d4f61f4909d0e41ef18e9afc3828
Connor Wilson <cnwilson29@gmail.com>
2021-09-01 11:59:24 -0400

    sponsors

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -4,6 +4,10 @@ import replit from './Sponsors/replit.png';
 import echoar from './Sponsors/echoAR.png';
 import xyz from './Sponsors/xyz.png';
 import voiceflow from './Sponsors/voiceflow.png';
+import axure from './Sponsors/axure.jpg';
+import uizard from './Sponsors/uizard.png';
+
+
 
 import './Sponsors.css'
 import './App.css';
@@ -16,46 +20,55 @@ function Sponsors() {
         }}></div>
         <div class="container sponsors">
             <div class="row">
-                <div class="col-6">
-                    <Sponsor 
-                        title='Wolfram'
-                        desc="Compute answers using Wolfram's breakthrough technology & knowledgebase, relied on by millions of students & professionals. "
-                        img={wolfram}
-                    >
-                    </Sponsor>
-                </div>
-                <div class="col-6">
-                    <Sponsor 
-                        title='Replit'
-                        desc='Replit is a simple yet powerful online IDE, Editor, Compiler, Interpreter, and REPL. Code, compile, run, and host in 50+ programming languages.'
-                        img={replit}
-                    >
-                    </Sponsor>
-                </div>
-                <div class="col-6">
-                    <Sponsor 
-                        title='EchoAR'
-                        desc='echoAR is a cloud-based 3D-first content management system (CMS) and delivery network (CDN) that provides server-side solutions to help scale augmented and virtual reality (AR/VR) applications.'
-                        img={echoar}
-                    >
-                    </Sponsor>
-                </div>
-                <div class="col-6">
-                    <Sponsor 
-                        title='XYZ'
-                        desc='.xyz is a bold, fresh choice for users who crave creativity and versatility in a domain name. It’s short and memorable without the limitations of a label or language barrier – so you can focus on connecting with your audience anywhere in the world.'
-                        img={xyz}
-                    >
-                    </Sponsor>
-                </div>
-                <div class="col-6">
-                    <Sponsor 
-                        title='Voiceflow'
-                        desc="Voiceflow helps teams create great conversations from start to finish. Bring your conversations to life with the platform used by the world's best teams."
-                        img={voiceflow}
-                    >
-                    </Sponsor>
-                </div>
+                <Sponsor 
+                    title='Wolfram'
+                    site='https://www.wolfram.com'
+                    desc="Compute answers using Wolfram's breakthrough technology & knowledgebase, relied on by millions of students & professionals. "
+                    img={wolfram}
+                >
+                </Sponsor>
+                <Sponsor 
+                    title='Replit'
+                    site='https://replit.com/'
+                    desc='Replit is a simple yet powerful online IDE, Editor, Compiler, Interpreter, and REPL. Code, compile, run, and host in 50+ programming languages.'
+                    img={replit}
+                >
+                </Sponsor>
+                <Sponsor 
+                    title='EchoAR'
+                    site='https://www.echoar.xyz'
+                    desc='echoAR is a cloud-based 3D-first content management system (CMS) and delivery network (CDN) that provides server-side solutions to help scale augmented and virtual reality (AR/VR) applications.'
+                    img={echoar}
+                >
+                </Sponsor>
+                <Sponsor 
+                    title='XYZ'
+                    site='https://gen.xyz'
+                    desc='.xyz is a bold, fresh choice for users who crave creativity and versatility in a domain name. It’s short and memorable without the limitations of a label or language barrier – so you can focus on connecting with your audience anywhere in the world.'
+                    img={xyz}
+                >
+                </Sponsor>
+                <Sponsor 
+                    title='Voiceflow'
+                    site='https://www.voiceflow.com'
+                    desc="Voiceflow helps teams create great conversations from start to finish. Bring your conversations to life with the platform used by the world's best teams."
+                    img={voiceflow}
+                >
+                </Sponsor>
+                <Sponsor 
+                    title='Axure'
+                    site='https://axure.com'
+                    desc="Axure RP is the only UX tool that gives UX professionals the power to build realistic, functional prototypes."
+                    img={axure}
+                >
+                </Sponsor>
+                <Sponsor 
+                    title='Uizard'
+                    site='https://uizard.io'
+                    desc="Uizard, the design tool for everyone: the world's first AI-powered digital design assistant for non-designers."
+                    img={uizard}
+                >
+                </Sponsor>
             </div>
         </div>
     </div>
@@ -64,13 +77,13 @@ function Sponsors() {
 
 function Sponsor(props) {
     return (
-        <div>
+        <div className='col-6 col-sm-4'>
             <img data-toggle="modal" data-target={'#' + props.title + 'Modal'} src={props.img} className='sponsor-image'></img>
-            <div class="modal fade" id={props.title + 'Modal'} tabindex="-1" role="dialog" aria-labelledby="echoARModalTitle" aria-hidden="true">
+            <div class="modal fade" id={props.title + 'Modal'} tabindex="-1" role="dialog" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title text-dark" id="exampleModalLongTitle">{props.title}</h5>
+                            <h5 class="modal-title text-dark" id="exampleModalLongTitle"><a href={props.site}>{props.title}</a></h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
