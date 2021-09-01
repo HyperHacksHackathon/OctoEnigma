6a317a9e7cf27a0f272250cd0fd3bf25806fa031
Pedram <55721066+Its-pedram@users.noreply.github.com>
2021-09-01 18:48:59 -0400

    Quick Update

--- src/Home.js
+++ src/Home.js
@@ -31,7 +31,7 @@ function Home(props) {
         }}
         >
         <g id="Group_10" data-name="Group 10" transform="translate(-1043.999 -832.069)">
-            <a href="https://bd8qlowg5rh.typeform.com/to/jPM6EAMU">
+            <a href="https://apply.hyperhacks.org">
             <path className="apply-button" id="Path_6" data-name="Path 6" d="M3378.629,3784.57H2972.5V3860.7l28.6,30h377.535Z" transform="translate(-1921 -2945)" fill={applyHoverState ? 'white' : 'black'} stroke="#ffffff" strokeWidth="15"/>
             <text id="APPLY" fill={applyHoverState ? 'black' : 'white'} transform="translate(1173 914)" fontSize="56" fontFamily="ibm-plex-sans, sans-serif" fontWeight="700">APPLY</text>
             </a>
